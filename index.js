3ad013e9a789c7d06b7569b382e7d41e3bdf7e5d
Benir2005 <beniromenda@gmail.com>
2025-04-09 08:54:19 +0300

    Java variables

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+let name='Madonna';
+console.log(name);
+// variables Cannot be a reserved KeyWord
+// Should be meaningful
+let firstName='Nir';
+let lastName='Eyal';
+console.log(firstName,lastName);
