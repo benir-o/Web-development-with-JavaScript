c6c8823b8d77c1f98101291269845843016e4903
Benir2005 <beniromenda@gmail.com>
2025-04-09 09:13:02 +0300

    JavaScript Functions introduction

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ console.log(name);
 let firstName='Nir';
 let lastName='Eyal';
 console.log(firstName,lastName);
+
+/*
+Arrays in JavaScript
+*/
+let selectedColors=['Blue','Green'];
+console.log(selectedColors);
+//Below is a JavaScript Function
+function greet(name){
+    console.log('Hello '+name)
+}
+greet('Brandon');
